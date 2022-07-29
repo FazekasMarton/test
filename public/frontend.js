b5634bb5327ae7a150acd1290155699b9f35001a
FazekasMarton <fazekas.marton16@gmail.com>
2022-07-29 17:27:03 +0200

    ...

---
+++ public/frontend.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        name: "Martin",
+        age: 12,
+        sex: "m"
+    },
+    {
+        name: "Zoe",
+        age: 18,
+        sex: "f" 
+    },
+    {
+        name: "Victor",
+        age: 34,
+        sex: "m"
+    }
+]
+module.exports = data;
