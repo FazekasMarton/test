743f7e9dcf204369af9bbeb6770bc5c6f2be3df7
FazekasMarton <fazekas.marton16@gmail.com>
2022-07-29 17:08:24 +0200

    first commit

---
+++ backend.js
@@ -0,0 +1,26 @@
+// Load modules.
+const express = require("express");
+const { url } = require("inspector");
+const path = require("path")
+const app = express()
+const port = process.env.PORT || 7000;
+const cors = require("cors");
+
+
+// Server static files.
+app.use(express.static("public"))
+app.use(cors())
+
+// Create new server.
+app.get("/", (req, res) => {
+    let indexPath = path.join(
+        __dirname,
+        "./html/index.html"
+    );
+    console.log(indexPath);
+    res.sendFile(indexPath);
+});
+// Listen port.
+app.listen(port, () => {
+    console.log("My app listen in: " + port);
+});
