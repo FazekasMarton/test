24ef40fb73b2298371bf656eb3d9b096909c6d19
FazekasMarton <fazekas.marton16@gmail.com>
2022-07-29 17:46:33 +0200

    ....

--- backend.js
+++ backend.js
@@ -3,9 +3,11 @@ const express = require("express");
 const { url } = require("inspector");
 const path = require("path")
 const app = express()
-const port = process.env.PORT || 7000;
+const port = 7000;
 const cors = require("cors");
 
+let data = require("./public/frontend.js");
+app.use(express.json())
 
 // Server static files.
 app.use(express.static("public"))
@@ -24,3 +26,21 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log("My app listen in: " + port);
 });
+
+const addListing = (newItem) => {
+    data = [...data, newItem]
+}
+
+const findListing = (id) => {
+    return data.find(item => item.id === Number(id))
+}
+
+app.get("/data", (req, res) => {
+    res.send(data)
+});
+
+app.post("/data", (req, res) => {
+    addListing(req, res)
+    res.status(201).send(data)
+});
+
