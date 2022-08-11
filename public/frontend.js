8e2320cc6fe8bef79dfb72020f9ab9bc5d4421f1
FazekasMarton <fazekas.marton16@gmail.com>
2022-08-11 13:54:10 +0200

    ...

--- public/frontend.js
+++ public/frontend.js
@@ -1,26 +1,8 @@
-const data = [
-    {
-        name: "Martin",
-        age: 12,
-        sex: "m"
-    },
-    {
-        name: "Zoe",
-        age: 18,
-        sex: "f" 
-    },
-    {
-        name: "Victor",
-        age: 34,
-        sex: "m"
-    }
-]
-function press(){
-
-fetch('https://guideianangel.herokuapp.com/data')
-    .then((response) => response.json())
-    .then((data) => function(){
-    document.getElementById("text").innerText=data["fname"]+" "+data["lname"];
-    console.log(data)
-    });
+var text = "empty"
+async function press(){
+await fetch('https://guideianangel.herokuapp.com/data')
+    .then(response => response.json())
+    .then(data => text=data["fname"]+" "+data["lname"]);
+    console.log(text)
+    document.getElementById("text").innerText=text;
 }
